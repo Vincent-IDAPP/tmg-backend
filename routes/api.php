c483530e61a6a32b8f8dac446e2734d8097d828e
Vincent Sechi <vincentsechi@MacBook-Pro-de-Vincent.local>
2025-06-20 15:08:51 +0200

    config

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,12 @@ use App\Http\Controllers\Api\UserController;
 
 
 Route::get('/users', [UserController::class, 'index']);
-
+Route::group([
+    'middleware' => 'api', // Applique les middlewares "api" : limite de requêtes (throttle), injection automatique de modèles, etc.
+], function () {
+    Route::get('/users', [UserController::class, 'index']);
+    
+});
 
 Route::get('/ping', function () {
     return response()->json(['pong' => true]);
