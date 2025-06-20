20d999d2f5217a682fb82e80ecd7c84c39fc6870
Vincent Sechi <vincentsechi@MacBook-Pro-de-Vincent.local>
2025-06-20 10:28:04 +0200

    add config

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
+class UserController extends Controller
+{
+    public function show($id): JsonResponse
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json($user);
+    }
+}
